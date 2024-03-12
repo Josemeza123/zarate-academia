0c27792c063eae2daec057fa2a02fb046fdd78c7
Jose Meza <enriquesais123@gmail.com>
2024-03-12 01:13:58 -0500

    estilos 1

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,17 @@
+import fb from "../assets/image/facebook.png";
+import insta from "../assets/image/instagram.png";
+import wsp from "../assets/image/whatsapp.png";
+import tiktok from "../assets/image/tik-tok.png";
+import yt from "../assets/image/youtube.png";
+import '../style/Footer.css'
+
 const Footer = () => {
   return (
     <div className="footer">
       <div className="sb__footer section__padding">
         <div className="sb__footer-links">
-          <div className="sb__footer-links-div">
-            <h4>actividad 1</h4>            
+          <div className="sb__footer-links_div">
+            <h4>actividad 1</h4>
             <a href="#">
               <p>Prueba</p>
             </a>
@@ -15,8 +22,8 @@ const Footer = () => {
               <p>Prueba</p>
             </a>
           </div>
-          <div className="sb__footer-links-div">
-            <h4>actividad 1</h4>            
+          <div className="sb__footer-links_div">
+            <h4>actividad 1</h4>
             <a href="#">
               <p>Prueba</p>
             </a>
@@ -27,8 +34,8 @@ const Footer = () => {
               <p>Prueba</p>
             </a>
           </div>
-          <div className="sb__footer-links-div">
-            <h4>actividad 2</h4>            
+          <div className="sb__footer-links_div">
+            <h4>actividad 2</h4>
             <a href="#">
               <p>Prueba</p>
             </a>
@@ -39,8 +46,8 @@ const Footer = () => {
               <p>Prueba</p>
             </a>
           </div>
-          <div className="sb__footer-links-div">
-            <h4>actividad 3</h4>            
+          <div className="sb__footer-links_div">
+            <h4>actividad 3</h4>
             <a href="#">
               <p>Prueba</p>
             </a>
@@ -51,8 +58,8 @@ const Footer = () => {
               <p>Prueba</p>
             </a>
           </div>
-          <div className="sb__footer-links-div">
-            <h4>actividad 4</h4>            
+          <div className="sb__footer-links_div">
+            <h4>actividad 4</h4>
             <a href="#">
               <p>Prueba</p>
             </a>
@@ -61,6 +68,54 @@ const Footer = () => {
             </a>
             <a href="#">
               <p>Prueba</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>logos</h4>
+            <div className="socialmedia">
+              <p>
+                <img src={fb} alt=""></img>
+              </p>
+              <p>
+                <img src={insta} alt=""></img>
+              </p>
+              <p>
+                <img src={wsp} alt=""></img>
+              </p>
+              <p>
+                <img src={tiktok} alt=""></img>
+              </p>
+              <p>
+                <img src={yt} alt=""></img>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="sb__footer-below">
+          <div className="sb__footer-copuright">
+            <p>@{new Date().getFullYear()} code all rigth reserved</p>
+          </div>
+          <div className="sb__footer-below-links">
+            <a href="#">
+              <div>
+                <p>Terminos y condiciones</p>
+              </div>
+            </a>
+            <a href="#">
+              <div>
+                <p>Terminos y condiciones</p>
+              </div>
+            </a>
+            <a href="#">
+              <div>
+                <p>Terminos y condiciones</p>
+              </div>
+            </a>
+            <a href="#">
+              <div>
+                <p>Terminos y condiciones</p>
+              </div>
             </a>
           </div>
         </div>
