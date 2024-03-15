1d141a200deca24bd6aecdee41c3bfc245353829
Jose Meza <enriquesais123@gmail.com>
2024-03-15 00:40:43 -0500

    slider-fin

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,10 +1,12 @@
 import "../style/Slider.css";
+import "../js/Slider.js";
 import img1 from "../assets/portadas/img1.jpg";
 import img2 from "../assets/portadas/img2.jpg";
 import img3 from "../assets/portadas/img3.jpg";
 import img4 from "../assets/portadas/img4.jpg";
 
 const Slider = () => {
+  
   return (
     <>
       <h1>Slider Infinito</h1>
@@ -23,8 +25,12 @@ const Slider = () => {
             <img src={img4} alt="" className="slider__img" />
           </div>
         </div>
-        <div className="slider__btn slider__btn--left" id="btn-rigth">⏪</div>
-        <div className="slider__btn slider__btn--rigth" id="btn-left">⏩</div>
+        <div className="slider__btn slider__btn--rigth" id="btn-left">
+          ⏩
+        </div>
+        <div className="slider__btn slider__btn--left" id="btn-rigth">
+          ⏪
+        </div>
       </div>
     </>
   );
---
+++ client/src/js/Slider.js
@@ -0,0 +1,38 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+const btnleft = document.querySelector("#btn-left");
+const btnrigth = document.querySelector("#btn-rigth");
+
+slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+
+function Previo() {
+  let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function () {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+function Next() {
+  let sliderSection = document.querySelectorAll(".slider__section");
+  let sliderSectionLast = sliderSection[sliderSection.length - 1];
+  slider.style.marginLeft = "0";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function () {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+btnleft.addEventListener('click',()=>{
+    Next()
+})
+
+
+console.log('hola mundo')
